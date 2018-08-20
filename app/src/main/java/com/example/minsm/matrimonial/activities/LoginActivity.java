c52a9da1b54ea33239c1dca4fe596780435e3bc0
Nivid Vaidya <nivid12@gmail.com>
2018-08-20 17:16:07 +0530

    Login url call added with structure.

--- app/src/main/java/com/example/minsm/matrimonial/activities/LoginActivity.java
+++ app/src/main/java/com/example/minsm/matrimonial/activities/LoginActivity.java
@@ -9,10 +9,27 @@ import android.widget.Toast;
 
 import com.example.minsm.matrimonial.R;
 import com.example.minsm.matrimonial.utils.DetectConnection;
+import com.example.minsm.matrimonial.utils.ProjectConstants;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    String mLogedinUsername, mLogedinPassword;
     FloatingActionButton buttonLogin;
+
+    private Call loginRequestCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +50,58 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    /**
+     * used for login service call
+     */
     private void loginServiceCall() {
 
         if(DetectConnection.checkInternetConnection(LoginActivity.this)) {
 
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            JSONObject jsonLoginResquest = new JSONObject();
+            try {
+                jsonLoginResquest.put(ProjectConstants.EMAIL, mLogedinUsername);
+                jsonLoginResquest.put(ProjectConstants.PASSWORD, mLogedinPassword);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            OkHttpClient clientLogin = new OkHttpClient();
+            HttpUrl.Builder urlBuilder = HttpUrl.parse(ProjectConstants.BASE_URL + ProjectConstants.VERSION_0 + ProjectConstants.LOGIN_URL).newBuilder();
+
+            Request requestLogin = new Request.Builder()
+                    .url(urlBuilder.build().toString())
+                    .post(RequestBody.create(MediaType.parse(ProjectConstants.APPLICATION_CHARSET), jsonLoginResquest.toString()))
+                    .build();
+
+            loginRequestCall = clientLogin.newCall(requestLogin);
+            loginRequestCall.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    if(!response.isSuccessful()) {
+
+                    } else {
+                        try {
+                            final JSONObject jsonLogin = new JSONObject(response.body().toString());
+                            final Boolean auth = jsonLogin.getBoolean(ProjectConstants.AUTH);
+                            final String message = jsonLogin.getString(ProjectConstants.MESSAGE);
+
+                            if(auth) {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+
+                        }
+                    }
+                }
+            });
 
         } else {
             Toast.makeText(LoginActivity.this, getResources().getString(R.string.check_internet), Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/minsm/matrimonial/utils/ProjectConstants.java
+++ app/src/main/java/com/example/minsm/matrimonial/utils/ProjectConstants.java
@@ -2,6 +2,26 @@ package com.example.minsm.matrimonial.utils;
 
 public class ProjectConstants {
 
+    //System wide constants
+    public static final String APPLICATION_CHARSET = "application/json; charset=utf-8";
+    public static final String AUTH = "auth";
+    public static final String MESSAGE = "msg";
+
+    public static final String BASE_URL = "http://metrimonial.it/api";
+    public static final String VERSION_0 = "/v0";
+    public static final String VERSION_1 = "/v1";
+
+    //Signup Activity constants
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+
+    public static final String SIGNUP_URL = "/register";
+
+
+    //Login Activity constants
+    public static final String LOGIN_URL = "/login";
+
 
 
 }
