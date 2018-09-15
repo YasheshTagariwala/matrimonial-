7659545757d9d54fbc83cb2cccddf4d03339c6a5
Mins <69hemin69@gmail.com>
2018-09-15 16:00:14 +0530

    added CardListAdapter for recyclerView with changed cell design

---
+++ app/src/main/java/com/kloudforj/matrimonial/RecyclerViewAdapters/CardListAdapter.java
@@ -0,0 +1,83 @@
+package com.kloudforj.matrimonial.RecyclerViewAdapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kloudforj.matrimonial.R;
+import com.kloudforj.matrimonial.activities.UserProfile;
+
+/**
+ * Created by minsm on 15-Sep-18.
+ */
+
+public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.ViewHolder> {
+
+    public Context context;
+
+    public CardListAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.userlist_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.setClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                context.startActivity(new Intent(context,UserProfile.class));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+        View view;
+
+        private ItemClickListener itemClickListener;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.view = view;
+            view.setOnClickListener(this);
+            view.setOnLongClickListener(this);
+        }
+
+        public void setClickListener(ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view, getAdapterPosition(), false);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            itemClickListener.onClick(view, getAdapterPosition(), true);
+            return true;
+        }
+
+        public View getView() {
+            return view;
+        }
+    }
+
+    public interface ItemClickListener {
+        void onClick(View view, int position, boolean isLongClick);
+    }
+
+}
--- app/src/main/java/com/kloudforj/matrimonial/activities/MainActivity1.java
+++ app/src/main/java/com/kloudforj/matrimonial/activities/MainActivity1.java
@@ -3,17 +3,21 @@ package com.kloudforj.matrimonial.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.kloudforj.matrimonial.R;
+import com.kloudforj.matrimonial.RecyclerViewAdapters.CardListAdapter;
 
 public class MainActivity1 extends AppCompatActivity {
 
     ImageButton imageButtonShare, imageButtonShare1, imageButtonShare2, imageButtonShare3;
     ImageView imageButtonOption, imageButtonOption1, imageButtonOption2, imageButtonOption3;
+    RecyclerView recyclerViewCardList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,16 @@ public class MainActivity1 extends AppCompatActivity {
         imageButtonOption1 = findViewById(R.id.imagebutton_option1);
         imageButtonOption2 = findViewById(R.id.imagebutton_option2);
         imageButtonOption3 = findViewById(R.id.imagebutton_option3);
+
+        recyclerViewCardList = findViewById(R.id.recyclerView_card_list);
+        recyclerViewCardList.setHasFixedSize(true);
+        recyclerViewCardList.setNestedScrollingEnabled(false);
+        recyclerViewCardList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//        recyclerViewCardList.addItemDecoration(new RecycleSpacing(25));
+        recyclerViewCardList.setItemViewCacheSize(100);
+        recyclerViewCardList.setDrawingCacheEnabled(true);
+        recyclerViewCardList.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        recyclerViewCardList.setAdapter(new CardListAdapter(this));
 
         imageButtonShare.setOnClickListener(new View.OnClickListener() {
             @Override
