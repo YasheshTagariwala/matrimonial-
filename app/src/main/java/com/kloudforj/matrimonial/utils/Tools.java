588deee9faf7e30eff4178d351f996614674a9ea
Mins <69hemin69@gmail.com>
2018-10-07 03:53:58 +0530

    ellis new-design for user profile

--- app/src/main/java/com/kloudforj/matrimonial/utils/Tools.java
+++ app/src/main/java/com/kloudforj/matrimonial/utils/Tools.java
@@ -3,7 +3,9 @@ package com.kloudforj.matrimonial.utils;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.DrawableRes;
+import android.support.v4.widget.NestedScrollView;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -24,5 +26,24 @@ public class Tools {
     public static int dpToPx(Context c, int dp) {
         Resources r = c.getResources();
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+    public static void nestedScrollTo(final NestedScrollView nested, final View targetView) {
+        nested.post(new Runnable() {
+            @Override
+            public void run() {
+                nested.scrollTo(500, targetView.getBottom());
+            }
+        });
+    }
+
+    public static boolean toggleArrow(View view) {
+        if (view.getRotation() == 0) {
+            view.animate().setDuration(200).rotation(180);
+            return true;
+        } else {
+            view.animate().setDuration(200).rotation(0);
+            return false;
+        }
     }
 }
