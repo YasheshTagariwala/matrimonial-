04c56e0474885a2cebc155435b90c21b2d318236
Yashesh Tagariwala <tagariwalayashesh@gmail.com>
2018-12-05 00:27:45 +0530

    Build No : YT05122018.1 Message : 1. Image Upload Camera Intent Crash Solved.

--- app/src/main/java/com/kloudforj/matrimonial/adapters/AdapterGridBasic.java
+++ app/src/main/java/com/kloudforj/matrimonial/adapters/AdapterGridBasic.java
@@ -9,7 +9,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -38,7 +40,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -258,8 +266,8 @@ public class AdapterGridBasic extends RecyclerView.Adapter<RecyclerView.ViewHold
         switch (requestCode) {
             case 0:
                 if (resultCode == RESULT_OK) {
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    mainHolder.imageButtonTimeline.setImageURI(selectedImage);
+                    Bitmap image = (Bitmap) imageReturnedIntent.getExtras().get("data");
+                    mainHolder.imageButtonTimeline.setImageBitmap(image);
                     mainHolder.imageButtonRemove.setVisibility(View.VISIBLE);
                     mainHolder.imageButtonTimeline.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -267,15 +275,106 @@ public class AdapterGridBasic extends RecyclerView.Adapter<RecyclerView.ViewHold
                         }
                     });
 
-                    //Uri selectedImage = imageReturnedIntent.getData();
-                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"CelebrationMatchMakers");
+                    if(!mediaStorageDir.exists()){
+                        if(!mediaStorageDir.mkdirs()){
+                            Log.e("Error","Cannot Create Directory");
+                        }
+                    }
 
-                    Cursor cursor1 = ctx.getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                    cursor1.moveToFirst();
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                    File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+                    try {
+                        FileOutputStream fileOutputStream = new FileOutputStream(mediaFile);
+                        image.compress(Bitmap.CompressFormat.JPEG,100,fileOutputStream);
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
-                    int columnIndex = cursor1.getColumnIndex(filePathColumn[0]);
-                    String picturePath = cursor1.getString(columnIndex);
-                    cursor1.close();
+                    LayoutInflater inflater = LayoutInflater.from(ctx);
+                    final View alertImageUpload = inflater.inflate(R.layout.layout_image_upload, null);
+                    ProgressBar mImageUploadProgressbar = alertImageUpload.findViewById(R.id.pb_image_upload);
+                    if (mImageUploadProgressbar != null) {
+                        mImageUploadProgressbar.getIndeterminateDrawable().setColorFilter(
+                                ContextCompat.getColor(ctx, R.color.colorAccent),
+                                android.graphics.PorterDuff.Mode.SRC_IN);
+                    }
+
+                    final AlertDialog.Builder builderInageUpload = new AlertDialog.Builder(ctx);
+                    builderInageUpload.setTitle("Uploading");
+                    builderInageUpload.setView(alertImageUpload);
+                    builderInageUpload.setCancelable(false);
+
+                    final AlertDialog alertDialogImageUpload = builderInageUpload.create();
+                    alertDialogImageUpload.show();
+
+                    Call requestCall;
+                    OkHttpClient client = new OkHttpClient();
+                    Request request;
+                    SharedPreferences globalSP = ctx.getSharedPreferences(ProjectConstants.PROJECTBASEPREFERENCE, MODE_PRIVATE);
+                    RequestBody req = new MultipartBody.Builder()
+                            .setType(MultipartBody.FORM)
+                            .addFormDataPart("user_id", String.valueOf(globalSP.getInt(ProjectConstants.USERID, 0)))
+                            .addFormDataPart("profile", mediaFile.getPath(), RequestBody.create(MediaType.parse("image/jpg"), mediaFile)).build();
+
+                    request = new Request.Builder()
+                            .url(ProjectConstants.BASE_URL + ProjectConstants.VERSION_0 + ProjectConstants.USER + ProjectConstants.IMAGE_UPLOAD_URL)
+                            .post(req)
+                            .header(ProjectConstants.APITOKEN, globalSP.getString(ProjectConstants.TOKEN, ProjectConstants.EMPTY_STRING))
+                            .build();
+
+                    Log.e("Request : ", request.toString());
+
+                    requestCall = client.newCall(request);
+                    requestCall.enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Log.e("Error : ", "123");
+                            Log.e("Error : ", e.getMessage());
+                            alertDialogImageUpload.dismiss();
+                            ((Activity) ctx).runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(ctx, "Error Uploading Image", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            try {
+                                String result = response.body().string();
+                                Log.e("Response : ", result);
+                                final JSONObject jsonLogin = new JSONObject(result);
+                                final Boolean auth = jsonLogin.getBoolean(ProjectConstants.AUTH);
+                                final String message = jsonLogin.getString(ProjectConstants.MESSAGE);
+                                if (auth) {
+                                    ((Activity) ctx).runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            alertDialogImageUpload.dismiss();
+                                            Toast.makeText(ctx, message, Toast.LENGTH_LONG).show();
+                                            SharedPreferences globalSP;
+                                            globalSP = ctx.getSharedPreferences(ProjectConstants.PROJECTBASEPREFERENCE, MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = globalSP.edit();
+                                            try {
+                                                editor.putString(ProjectConstants.BASE_IMAGE, jsonLogin.getString(ProjectConstants.BASE_IMAGE));
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                            editor.apply();
+                                        }
+                                    });
+                                }
+                            } catch (JSONException e) {
+                                alertDialogImageUpload.dismiss();
+                                e.printStackTrace();
+                            }
+                        }
+                    });
                 }
                 break;
 
